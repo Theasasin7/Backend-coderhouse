16155d20fdb151b84b53129083f8f53fb5a7a90f
Theasasin7 <ulises.cruz7@hotmail.com>
2022-06-23 18:13:02 -0400

    ejercicios de practica de clase 19

---
+++ Clase 19/index_ej2.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+
+const ejecutar = async () => {
+
+  await mongoose.connect("mongodb://localhost:27017/estudiantes");
+
+  const estudSchema = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    apellido: { type: String, required: true },
+    edad: { type: Number, required: true },
+    dni: { type: String, required: true, unique: true },
+    curso: { type: String, required: true },
+    nota: { type: Number, required: true }
+  });
+
+  const Estudiante = mongoose.model("estud", estudSchema);
+
+  /*
+  let res = await Estudiante.find().sort({ nombre: 1 });
+  console.log(res);
+  */
+  
+  /*
+  res = await Estudiante.find().sort({ edad: 1 }).limit(1);
+  console.log(res);*/
+
+  /*
+  let res = await Estudiante.find({ curso: "2A" });
+  console.log(res);
+  */
+
+  /*
+  let res = await Estudiante.find().sort({ edad: 1 }).skip(1).limit(1);
+  console.log(res);
+  */
+
+  /*
+  let res = await Estudiante.find({}, {nombre: 1, apellido: 1, curso: 1, _id: 0}).sort({ apellido: -1 });
+  console.log(res);
+  */
+
+  /*
+  let res = await Estudiante.find({ nota: 10 });
+  console.log(res);
+  */
+
+  let res = await Estudiante.aggregate([
+    {
+      $group: {
+        "_id": null,
+        "promedio": {
+          $avg: "$nota"
+        }
+      }
+    }
+  ]);
+
+  console.log(res);
+
+  console.log("Proceso terminado");
+
+}
+
+ejecutar();
