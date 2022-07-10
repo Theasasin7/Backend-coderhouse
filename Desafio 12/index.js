a2144b6137318f81bce4e94dffb58f075c4b58e0
Theasasin7 <ulises.cruz7@hotmail.com>
2022-07-10 13:01:15 -0400

    desafio 12 de clase 24

---
+++ Desafio 12/index.js
@@ -0,0 +1,33 @@
+const { urlencoded } = require("express");
+const express = require("express");
+const app = express();
+let port = 8080;
+
+let productArray = [];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/productos", (req, res) => {
+  res.render("index.pug");
+});
+app.post("/productos", (req, res) => {
+  const { title, price, thumbnail } = req.body;
+  const newProduct = {
+    title: title,
+    price: price,
+    thumbnail: thumbnail,
+  };
+  productArray.push(newProduct);
+  res.render("index.pug", { products: productArray });
+});
+
+const server = app.listen(port, (req, res) => {
+  console.log("SERVER RUNNING ON PORT: ", port);
+});
+
+server.on("error", (err) => {
+  console.log("ERROR!!: ", er);
+});
