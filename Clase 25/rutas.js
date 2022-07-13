099f0126541ea37f485ebdab69d83cc430658e4c
Theasasin7 <ulises.cruz7@hotmail.com>
2022-07-12 23:04:54 -0400

    ejercicios de practica de clase 25

---
+++ Clase 25/rutas.js
@@ -0,0 +1,14 @@
+
+
+const bienvenida = (req, res, next) => {
+  res.send('Bienvenido!');
+};
+
+const registracion = (req, res, next) => {
+  res.send('Registrado Correctamente!');
+};
+
+module.exports = {
+  bienvenida,
+  registracion
+}
